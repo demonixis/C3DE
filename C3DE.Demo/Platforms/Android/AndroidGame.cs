be7060b7d3b42e8292aaccf7c47bb62ed130734b
Yannick Comte <ycomte@wanadev.fr>
2014-09-30 13:51:53 +0200

    Added Android Support, it's still work in progress but with some works it'll be great!

---
+++ C3DE.Demo/Platforms/Android/AndroidGame.cs
@@ -0,0 +1,24 @@
+﻿using C3DE.Demo.Scenes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace C3DE.Demo
+{
+    public class AndroidGame : Engine
+    {
+		public AndroidGame() : base() { 
+
+		}
+
+        protected override void Initialize()
+        {
+            Application.SceneManager.Add(new MenuDemo(), true);
+            Application.SceneManager.Add(new HeightmapDemo());
+            Application.SceneManager.Add(new RandomTerrain());
+            Application.SceneManager.Add(new GridDemo());
+            Application.SceneManager.Add(new HexagonTerrainDemo());
+            Application.LoadLevel(1);
+            base.Initialize();
+        }
+    }
+}
---
+++ C3DE.Demo/Platforms/Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace C3DE.Demo
+{
+    [Activity(Label = "C3DE.Demo"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState=true
+        , LaunchMode=Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class MainActivity : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var g = new AndroidGame();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+    }
+}
+
