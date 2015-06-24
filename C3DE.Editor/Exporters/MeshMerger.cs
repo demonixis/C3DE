7d278f840529401976ea51d6876a4bf39ea89d8b
Yannick <comte.yannick@gmail.com>
2015-06-24 07:03:12 +0200

    Added a mesh merger

---
+++ C3DE.Editor/Exporters/MeshMerger.cs
@@ -0,0 +1,63 @@
+﻿using C3DE.Components.Renderers;
+using C3DE.Geometries;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3DE.Editor.Exporters
+{
+    public sealed class MeshMerger
+    {
+        public static Geometry Merge(MeshRenderer[] renderers)
+        {
+            var positions = new List<Vector3>();
+            var normals = new List<Vector3>();
+            var uvs = new List<Vector2>();
+            var indices = new List<ushort>();
+
+            Geometry current = null;
+            Vector3[] p = null;
+            int j = 0;
+            int k = 0;
+
+            for (int i = 0, l = renderers.Length; i < l; i++)
+            {
+                current = renderers[i].Geometry;
+
+                p = current.GetVertices(VertexType.Position);
+                for (j = 0, k = p.Length; j < k; j++)
+                    p[j] = Vector3.Transform(p[j], renderers[i].Transform.WorldMatrix);
+
+                positions.AddRange(p);
+
+                p = current.GetVertices(VertexType.Normal);
+                for (j = 0, k = p.Length; j < k; j++)
+                    p[j] = Vector3.TransformNormal(p[j], renderers[i].Transform.WorldMatrix);
+                
+                normals.AddRange(p);
+
+                uvs.AddRange(current.GetUVs());
+                indices.AddRange(current.Indices);
+            }
+
+            var size = positions.Count;
+            current = new Geometry();
+            current.Vertices = new VertexPositionNormalTexture[size];
+            
+            for (int i = 0; i < size; i++)
+            {
+                current.Vertices[i].Position = positions[i];
+                current.Vertices[i].Normal = normals[i];
+                current.Vertices[i].TextureCoordinate = uvs[i];
+            }
+
+            current.Indices = indices.ToArray();
+
+            return current;
+        }
+    }
+}
--- C3DE.Editor/Exporters/STLExporter.cs
+++ C3DE.Editor/Exporters/STLExporter.cs
@@ -1,4 +1,5 @@
-﻿using C3DE.Components.Renderers;
+﻿using C3DE.Components;
+using C3DE.Components.Renderers;
 using C3DE.Geometries;
 using Microsoft.Xna.Framework;
 using System;
@@ -10,30 +11,43 @@ namespace C3DE.Editor.Exporters
     {
         public static float Precision = 100.0f;
 
-        public static string Export(SceneObject sceneObject)
+        public static string MergeExport(MeshRenderer[] renderers)
         {
-            var renderer = sceneObject.GetComponent<MeshRenderer>();
+            var geometry = MeshMerger.Merge(renderers);
+            var renderer = new MeshRenderer();
+            renderer.Transform = new Transform();
+            renderer.Geometry = geometry;
+            return Export(renderer);
+        }
+
+        public static string Export(MeshRenderer renderer)
+        {
             if (renderer != null && renderer.Geometry != null)
             {
                 var geometry = renderer.Geometry;
                 var positions = geometry.GetVertices(VertexType.Position);
                 var normals = geometry.GetVertices(VertexType.Normal);
                 var indices = geometry.Indices;
-                var world = sceneObject.Transform.WorldMatrix;
+                var world = renderer.Transform.WorldMatrix;
                 var stringBuilder = new StringBuilder();
+                // Caches
+                var v1 = Vector3.Zero;
+                var v2 = Vector3.Zero;
+                var v3 = Vector3.Zero;
+                var normal = Vector3.Zero;
 
                 for (int i = 0, l = indices.Length; i < l; i += 3)
                 {
-                    var v1 = Vector3.Transform(positions[i], world);
-                    var v2 = Vector3.Transform(positions[i + 1], world);
-                    var v3 = Vector3.Transform(positions[i + 2], world);
-                    var normal = Vector3.TransformNormal(normals[i], world);
+                    v1 = Vector3.Transform(positions[i], world);
+                    v2 = Vector3.Transform(positions[i + 1], world);
+                    v3 = Vector3.Transform(positions[i + 2], world);
+                    normal = Vector3.TransformNormal(normals[i], world);
 
-                    stringBuilder.Append("\tfacet normal " + RoundF(normal.X) + " " + RoundF(normal.Y) + " " + RoundF(normal.Z) + "\n");
+                    stringBuilder.AppendFormat("\tfacet normal {0} {1} {2}\n", RoundF(normal.X), RoundF(normal.Y), RoundF(normal.Z));
                     stringBuilder.Append("\t\touter loop\n");
-                    stringBuilder.Append("\t\t\tvertex " + RoundF(v1.X) + " " + RoundF(v1.Y) + " " + RoundF(v1.Z) + "\n");
-                    stringBuilder.Append("\t\t\tvertex " + RoundF(v2.X) + " " + RoundF(v2.Y) + " " + RoundF(v2.Z) + "\n");
-                    stringBuilder.Append("\t\t\tvertex " + RoundF(v3.X) + " " + RoundF(v3.Y) + " " + RoundF(v3.Z) + "\n");
+                    stringBuilder.AppendFormat("\t\t\tvertex {0} {1} {2}\n", RoundF(v1.X), RoundF(v1.Y), RoundF(v1.Z));
+                    stringBuilder.AppendFormat("\t\t\tvertex {0} {1} {2}\n", RoundF(v2.X), RoundF(v2.Y), RoundF(v2.Z));
+                    stringBuilder.AppendFormat("\t\t\tvertex {0} {1} {2}\n", RoundF(v3.X), RoundF(v3.Y), RoundF(v3.Z));
                     stringBuilder.Append("\t\tendloop\n");
                     stringBuilder.Append("\tendfacet\n");
                 }
